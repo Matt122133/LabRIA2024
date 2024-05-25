52bd9ede03f93e40aa3c4d942370a2e936cbcce6
Mateo Tambasco <mateo.tambasco@cencosud.com>
2024-05-24 21:51:01 -0300

    env: se agrega variable de entorno

--- src/Products/helpers/getProductsApi.js
+++ src/Products/helpers/getProductsApi.js
@@ -1,5 +1,5 @@
 export const getProductsApi = async () => {
-  const data = await fetch('https://fakestoreapi.com/products');
+  const data = await fetch(import.meta.env.VITE_APP_FAKE_STORE_API_URL);
   const products = await data.json();
   return products;
 };
