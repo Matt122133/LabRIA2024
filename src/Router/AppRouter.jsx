083c8bdd1908b6df105109063678e7022adbb839
Angelo Giroldi <agiroldi45@gmail.com>
2024-05-26 16:58:47 -0300

    branchLogin: add new page Login

--- src/Router/AppRouter.jsx
+++ src/Router/AppRouter.jsx
@@ -1,12 +1,12 @@
 import { Route, Routes } from "react-router-dom";
-import { Login } from "../Auth/components/Login";
 import { ProductRoutes } from "../Products/routes/ProductRoutes";
 import { ProductDetailPage } from "../Products/Page/ProductDetailPage";
+import Login from "../Auth/components/Login";
 export const AppRouter = () => {
   return (
     <>
       <Routes>
-        <Route path="/login" element={<Login />} />
+        <Route path="/login" element={<Login/>} />
         <Route path="/detail/:id" element={<ProductDetailPage />} />
         <Route path="*" element={<ProductRoutes />} />
       </Routes>
