cf693ff6508dfc89f0db7ac2149653015763cabf
Mateo Tambasco <mateo.tambasco@cencosud.com>
2024-05-31 19:51:53 -0300

    newApi: add new reestructuration, and new api fetch

--- src/Products/helpers/getProductsApi.js
+++ src/Products/helpers/getProductsApi.js
@@ -1,11 +1,11 @@
 export const getProductsApi = async () => {
-  const data = await fetch(import.meta.env.VITE_APP_FAKE_STORE_API_URL);
+  const data = await fetch(import.meta.env.VITE_APP_DUMMY_JSON_API_URL);
   const products = await data.json();
   return products.products;
 };
 
 export const getProductsApiById = async (productId) => {
-  const data = await fetch(`${import.meta.env.VITE_APP_FAKE_STORE_API_URL}/${productId}`);
+  const data = await fetch(`${import.meta.env.VITE_APP_DUMMY_JSON_API_URL}/${productId}`);
   const product = await data.json();
   return product;
 }
