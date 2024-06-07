6b42a8e25ede166b48985f07821dbb76d8180329
Angelo Giroldi <agiroldi45@gmail.com>
2024-06-06 22:57:49 -0300

    Sidebar Filters Category

---
+++ src/Products/hooks/useFetchProductsByCategory.js
@@ -0,0 +1,21 @@
+import { useEffect, useState } from "react";
+import { getProductsByCategory } from '../helpers/getProductsApi.js';
+
+export const useFetchProductsByCategory = (category) => {
+
+    const [categories, setCategories] = useState([]);
+  
+    const getProductsCategory = async () => {
+      const products = await getProductsByCategory(category);
+      setCategories(products);
+    }
+  
+    useEffect( () => {
+        getProductsCategory();
+    }, []);
+  
+    return {
+        categories
+    };
+  
+}
