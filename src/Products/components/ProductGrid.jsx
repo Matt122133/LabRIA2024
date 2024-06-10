6c0df5c4e59e64d806e07baf9d2ed59160f0c5c0
Mateo Tambasco <mateo.tambasco@cencosud.com>
2024-06-10 13:19:05 -0300

    final commit

--- src/Products/components/ProductGrid.jsx
+++ src/Products/components/ProductGrid.jsx
@@ -2,15 +2,7 @@ import { Grid } from "@mui/material";
 import { Product } from "./Product";
 
 export const ProductGrid = ({ products }) => (
-  <Grid
-    item
-    xs={12}
-    mb={2}
-    mt={4}
-    container
-    justifyContent="flex-start"
-    gap="20px"
-  >
+  <Grid item xs={12} mb={2} mt={4} container justifyContent="center" gap="20px">
     {products.map((product) => (
       <Product key={product.id} {...product} />
     ))}
